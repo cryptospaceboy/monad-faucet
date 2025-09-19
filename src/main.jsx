3c86b8e5588b4a27014c3927547f2268d3e34aec
cryptospaceboy <perfecteguare@gmail.com>
2025-09-19 01:56:11 +0100

    maintainance

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'; // Make sure file name is exactly "App.jsx"
-import './index.css'; // Global styles
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // optional global styles
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
